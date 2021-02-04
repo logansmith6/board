8f2855c5a82a88fd7c3b1dd30c620237df987e3c
logansmith6 <logsmith06@gmail.com>
2021-02-04 12:33:18 -0800

    added constructor to user class

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -1,1 +1,7 @@
-//
+class Photo{
+    constructor(username, email, password_digest){
+        this.username = username;
+        this.email = email;
+        this.password_digest = password_digest;
+    }
+}
