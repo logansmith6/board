1eeb356352dab92ecfd1ea858c6cb6da04ca43ec
logansmith6 <logsmith06@gmail.com>
2021-02-04 12:46:11 -0800

    added fetchusers function to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMCOntentLoaded", () => {
     debugger;
+
+    const BASE_URL = "http://127.0.0.1:3000"
+    // list all users
+
+    function fetchUsers(){
+        fetch(`${BASE_URL}/users`)
+    }
+
+    //create new users
+
+    //delete users
 })
