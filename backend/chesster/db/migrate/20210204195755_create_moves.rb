f38c35973cc2d3464f7de195860cef1fa1b6d191
logansmith6 <logsmith06@gmail.com>
2021-02-04 12:04:11 -0800

    added move dependencies to createmoves table

--- backend/chesster/db/migrate/20210204195755_create_moves.rb
+++ backend/chesster/db/migrate/20210204195755_create_moves.rb
@@ -1,7 +1,8 @@
 class CreateMoves < ActiveRecord::Migration[6.0]
   def change
     create_table :moves do |t|
-
+      t.belongs_to :user
+      t.belongs_to :game
       t.timestamps
     end
   end
