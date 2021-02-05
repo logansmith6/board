3517bbe6d696f2c37e594a6a09c2939e42cdbb4f
logansmith6 <logsmith06@gmail.com>
2021-02-04 17:56:40 -0800

    final adjustments to login aesthetics

--- frontend/src/login/registration.js
+++ frontend/src/login/registration.js
@@ -7,6 +7,12 @@ function signup(){
     document.querySelector(".login-form-container").style.cssText="display: none";
     document.querySelector(".register-form-container").style.cssText="display: block";
     document.querySelector(".button-1").style.cssText="display: none";
-    
     document.querySelector(".button-2").style.cssText="display: block";
+}
+
+function login(){
+    document.querySelector(".login-form-container").style.cssText="display: block";
+    document.querySelector(".register-form-container").style.cssText="display: none";
+    document.querySelector(".button-1").style.cssText="display: block margin-left: 95px";
+    document.querySelector(".button-2").style.cssText="display: none";
 }
